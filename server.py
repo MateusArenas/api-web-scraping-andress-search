7a309056eadde40ae4a906e0d2d553210fdec6c1
MateusArenas <MateusArenas97@gmail.com>
2022-10-31 00:02:26 -0300

    more

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 # base https://www.procep.com.br
 # search https://www.procep.com.br//localizar//r-antonia-de-oliveira-n53-jardim-maria-eneida-maua-sp
@@ -7,8 +7,12 @@ from markupsafe import escape
 
 from andress import search_andresses
 
-app = Flask(__name__)
+app = Flask(__name__,  template_folder='pages')
+
+@app.route("/")
+def index():
+  return render_template('index.html')
 
 @app.route("/<search>")
-def hello_world(search):
+def search(search):
     return search_andresses(escape(search))
